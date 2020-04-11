8396047f1a229bb862c3902e9fb1a37207bb73d1
khazuki <curtis.p.fong@gmail.com>
2020-04-11 15:03:43 -0700

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,12 +1,26 @@
-import os
+while True:
+	answer = input("Will this be a self-bot, running on your personal account (this is against Discord TOS)? y/n\n")
 
-os.system('cmd /c "pip install discord urlextract"')
+	if answer.startswith("y"):
+		selfbot = True
+		break
 
-user_id=input("Input your Discord user id: ")
-user_token=input("Input your Discord user token: ")
+	elif answer.startswith("n"):
+		selfbot = False
+		break
+
+if selfbot:
+	secret = input("Input your account's secret.")
+else:
+	secret = input("Input your bot's secret (available in the discord developer portal):\n")
+
+owner_id = input("Input your discord id, used to identify you as the bot's owner:\n")
+bot_testing_guild_id = input("Input the id of the server you will use for testing your bot:\n")
+bot_testing_channel_id = input("Input the id of the channel you will use for testing your bot:\n")
+prefix = input("Input the prefix your bot will use for its commands:\n")
 
 f=open("auth.py", "w")
-f.write("my_id = %s\ntoken = '%s'"%(user_id, user_token))
+f.write("secret = '%s'\nowner_id = %d\nbot_testing_guild_id = %d\nbot_testing_channel_id = %d\nprefix = '%s'\nselfbot = %s"%(secret, int(owner_id), int(bot_testing_guild_id), int(bot_testing_channel_id), prefix, str(selfbot)))
 f.close()
 
-input("Setup complete. Run bot.py to start the bot. Hit enter to close this window.")
+input("Setup complete. Run main.pyw to start the bot. Hit enter to close this window.")
